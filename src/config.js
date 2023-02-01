a6640ae5a1706aa20ded5eca2742f0363ff9d899
uber harley cardoso <hhcardoso@misena.edu.co>
2023-02-01 14:46:09 -0500

    Add port connection db

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import "dotenv/config"; /* const dotenv = require('dotenv').config() */
 
 export const PORT = process.env.PORT || 3020;
 
+export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_NAME = process.env.DB_NAME || "usersdb";
 export const DB_USER = process.env.DB_USER || "adminAnonymous";
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "passwordRoot";
-export const DB_NAME = process.env.DB_NAME || "usersdb";
-export const DB_PORT = process.env.DB_PORT || 3306;
--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
-import { DB_USER, DB_HOST, DB_PASSWORD, DB_NAME } from "./config.js";
+import { DB_USER, DB_HOST, DB_PASSWORD, DB_NAME, DB_PORT } from "./config.js";
 import { createPool } from "mysql2/promise";
 
 export const poll = createPool({
   database: DB_NAME,
   user: DB_USER,
   host: DB_HOST,
+  port: DB_PORT,
   password: DB_PASSWORD,
   ssl: {
     rejectUnauthorized: false,
